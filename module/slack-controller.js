92334b2e27d3d5803523cb6169c8e505a86430c3
Takuma Hanatani <mail@potato4d.me>
2016-07-29 22:08:07 +0900

    Fixed Lint error slack-controller.js

--- module/slack-controller.js
+++ module/slack-controller.js
@@ -1,4 +1,4 @@
-const slackController = function(params) {
+const slackController = function() {
 	this.token = "";
 	this.baseUrl = "https://slack.com/api/";
 	this.endpoint = "";
@@ -6,16 +6,19 @@ const slackController = function(params) {
 
 	this.setAuthData = (token) => {
 		this.token = token;
-	}
+	};
 
 	this.makeUri = () => {
 		return this.baseUrl + this.endpoint + "?token=" + this.token + "&pretty=1";
-	}
+	};
 
 	this.getChannelsList = () => {
 		return new Promise( (resolve, reject) => {
 			this.endpoint = "channels.list";
 			this.request(this.makeUri(), (err, res, body) => {
+				if(err) reject(err);
+				if(res.statusCode != 200) reject(res.statusCode);
+
 				resolve(JSON.parse(body).channels);
 			});
 		});
@@ -25,10 +28,13 @@ const slackController = function(params) {
 		return new Promise( (resolve, reject) => {
 			this.endpoint = "users.list";
 			this.request(this.makeUri(), (err, res, body) => {
+				if(err) reject(err);
+				if(res.statusCode != 200) reject(res.statusCode);
+
 				resolve(JSON.parse(body).members);
 			});
 		});
 	};
-}
+};
 
-module.exports = params => {return new slackController(params);};
+module.exports = () => {return new slackController();};
--- test/index.js
+++ test/index.js
@@ -5,5 +5,5 @@ const assert = require("assert");
 describe("Pass", () => {
 	it("Assert", ()=>{
 		assert(true);
-	})
+	});
 });
